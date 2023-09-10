5da2efd4b4d0d660dc5571e60d7c51943ce37629
Andrey <66728580+Hadez-Kos@users.noreply.github.com>
2023-09-10 14:42:03 +0300

    refs #2, Add and connection database postgresql in project

--- resumes/config.py
+++ resumes/config.py
@@ -4,3 +4,8 @@ from dotenv import load_dotenv
 load_dotenv()
 
 secret_key = os.getenv("secret_key")
+db_port = os.getenv("db_port")
+db_host = os.getenv("db_host")
+db_database = os.getenv("db_database")
+db_user = os.getenv("db_user")
+db_password = os.getenv("db_password")
