5e3b726c51887c48aa49d5f323c8d1055499c3d0
Andrey <66728580+Hadez-Kos@users.noreply.github.com>
2023-09-10 20:06:04 +0300

    refs #3, Add and connection database Models

--- resumes/personal/models.py
+++ resumes/personal/models.py
@@ -1,3 +1,54 @@
 from django.db import models
 
+
 # Create your models here.
+class User(models.Model):
+    email = models.EmailField(
+        verbose_name='Email адрес', max_length=255, unique=True
+    )
+    last_name = models.CharField(
+        verbose_name='Фамилия', max_length=150, blank=False
+    )
+    first_name = models.CharField(
+        verbose_name='Имя', max_length=150, blank=False
+    )
+    middle_name = models.CharField(
+        verbose_name='Отчество', max_length=150, blank=True
+    )
+    phone = models.CharField(
+        verbose_name='Телефон', max_length=20, blank=False
+    )
+    birthday = models.DateField(
+        verbose_name='Дата рождения', blank=False
+    )
+    password = models.CharField(
+        verbose_name='Пароль', max_length=200, blank=False
+    )
+
+    @property
+    def full_name(self):
+        return f"{self.last_name} {self.first_name} {self.middle_name}"
+
+    def __repr__(self):
+        return f"{self.full_name} {self.email}"
+
+    class Meta:
+        verbose_name = "Пользователь"
+        verbose_name_plural = "Пользователи"
+
+
+class ProfessionChoices(models.Model):
+    profession = models.CharField(
+        verbose_name='Профессия', max_length=150, blank=False
+    )
+    position = models.CharField(
+        verbose_name='Должность', max_length=150, blank=False
+    )
+    user = models.ForeignKey(User, on_delete=models.CASCADE, verbose_name="Пользователь")
+
+    def __repr__(self):
+        return f"{self.profession} {self.position}"
+
+    class Meta:
+        verbose_name = "Профессия"
+        verbose_name_plural = "Профессии"
